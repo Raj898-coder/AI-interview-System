5d006ccce519fa8528e3c6c070b0a720a7a80d92
Raj898-coder <santoshrajstm2023@gmail.com>
2026-05-11 20:17:18 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
-import { useEffect } from 'react'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
 import { setUserData } from './redux/userSlice'
@@ -21,19 +20,33 @@ export const ServerUrl  = "https://ai-interview-system-0uxu.onrender.com"
 function App() {
 
   const dispatch = useDispatch()
-  useEffect(()=>{
+
+  useEffect(() => {
+
     const getUser = async () => {
+
       try {
-        const result = await axios.get(ServerUrl + "/api/user/current-user", {withCredentials:true})
-        dispatch(setUserData(result.data))
+
+        const result = await axios.get(
+          ServerUrl + "/api/user/current-user",
+          { withCredentials:true }
+        )
+
+        // SIRF USER SAVE KARO
+        dispatch(setUserData(result.data.user))
+
       } catch (error) {
+
         console.log(error)
+
         dispatch(setUserData(null))
       }
     }
+
     getUser()
 
-  },[dispatch])
+  }, [dispatch])
+
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
@@ -47,9 +60,6 @@ function App() {
       <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       <Route path="/refund-policy" element={<RefundPolicy />} />
       <Route path="/contact" element={<Contact />} />
-
-
-
     </Routes>
   )
 }
